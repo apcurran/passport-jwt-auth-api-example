7dc15a4866995f3ede86939b694dd48360cae67e
Alex Curran <alexcurran.va@gmail.com>
2024-07-25 15:49:19 -0500

    General server error handler

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const app = express();
 // middleware to parse JSON req bodies
 app.use(express.json());
 
+// general server error handler
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).json({ error: "Something went wrong on the server." });
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}.`);
 });
