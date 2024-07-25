d052e2550b25a7f8d81ae42e0a442ef6c12493ac
Alex Curran <alexcurran.va@gmail.com>
2024-07-25 15:38:18 -0500

    Add JSON parsing of request bodies with express.json() middleware

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require("express");
 
 const app = express();
 
+// middleware to parse JSON req bodies
+app.use(express.json());
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}.`);
 });
