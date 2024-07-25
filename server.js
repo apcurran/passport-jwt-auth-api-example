6d5145c8853316aa5f258ab9c7c7c25cd4cb9af7
Alex Curran <alexcurran.va@gmail.com>
2024-07-25 15:35:14 -0500

    Base express server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const express = require("express");
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}.`);
+});
