7406f299dfe1c0623e77025ee7d26f615e5fd4e2
Alex Curran <alexcurran.va@gmail.com>
2024-08-05 15:49:35 -0500

    Check for pre-existing user account during sign-up logic

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -8,6 +8,16 @@ const { db } = require("../db/index");
 async function postUserSignup(req, res, next) {
     try {
         const { email, password } = req.body;
+        // check first for pre-existing user account
+        const emailExists = await db.oneOrNone(`
+            SELECT app_user.user_id
+            FROM app_user
+            WHERE app_user.email = $<email>
+            `, { email });
+
+        if (emailExists) {
+            return res.status(400).json({ error: "Email already exists." });
+        }
 
         res.json({ message: "Success! You created an account!" });
     }
