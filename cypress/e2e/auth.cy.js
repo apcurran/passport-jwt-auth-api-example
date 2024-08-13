4f5118e2753f4b2682fd683e2c022b645be5c698
Alex Curran <alexcurran.va@gmail.com>
2024-08-13 16:28:07 -0500

    Cypress test for user sign-up process

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,10 +1,14 @@
 describe("Auth", () => {
+    before(() => {
+        cy.task("resetDbUsers");
+    });
+
     it("creates a new user with an email and password", () => {
         cy.request("POST", "/auth/sign-up", { email: "bobtest@example.com", password: "testing123" })
-            .then(
-                (response) => {
-                    expect(response.body).to.include("Success! You created an account!");
-                },
-            );
+            .then((response) => {
+                expect(response.status).to.eq(201);
+
+                expect(response.body).to.deep.equal({ message: "Success! You created an account!" });
+            });
     });
 });
