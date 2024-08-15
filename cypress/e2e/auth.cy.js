325cc9267a1ce736060a805c39ac0892945bff41
Alex Curran <alexcurran.va@gmail.com>
2024-08-15 16:24:19 -0500

    Change eq to equal method on Cy expectation for consistency

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -7,7 +7,7 @@ describe("Auth", () => {
     it("creates a new user with an email and password", () => {
         cy.request("POST", "/auth/sign-up", { email: "bobtest@example.com", password: "testing123" })
             .then((response) => {
-                expect(response.status).to.eq(201);
+                expect(response.status).to.equal(201);
 
                 expect(response.body).to.deep.equal({ message: "Success! You created an account!" });
             });
@@ -17,7 +17,7 @@ describe("Auth", () => {
         cy.request({ method: "POST", url: "/auth/sign-up", body: { email: "bobtest@example.com", password: "testing123" }, failOnStatusCode: false })
             .then((response) => {
                 // client error
-                expect(response.status).to.eq(400);
+                expect(response.status).to.equal(400);
 
                 expect(response.body).to.deep.equal({ error: "Email already exists." });
             });
@@ -32,7 +32,7 @@ describe("Auth", () => {
             failOnStatusCode: false,
         }).then((response) => {
             // client error
-            expect(response.status).to.eq(400);
+            expect(response.status).to.equal(400);
 
             expect(response.body).to.deep.equal({ error: "Email is not found." });
         });
