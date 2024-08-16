a904221613daa49f2d2ad40b48d8659fd8d7789c
Alex Curran <alexcurran.va@gmail.com>
2024-08-16 15:27:13 -0500

    Add basic success message for user route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,7 +2,11 @@
 
 /** @type {import("express").RequestHandler} */
 async function getProtected(req, res, next) {
-
+    try {
+        res.status(200).json({ message: "You successfully accessed the protected route user!" });
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
