8b7d1f85e01c528349302b894fa0227c8f1be1a8
Alex Curran <alexcurran.va@gmail.com>
2024-08-16 15:37:25 -0500

    Enable route protection with Passport JS JWT auth

--- routes/user-router.js
+++ routes/user-router.js
@@ -2,9 +2,10 @@
 
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const userController = require("../controllers/user-controller");
 
-router.get("/protected", userController.getProtected);
+router.get("/protected", passport.authenticate("jwt", { session: false }), userController.getProtected);
 
 module.exports = router;
