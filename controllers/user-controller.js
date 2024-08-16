e3729b5bbfcd2b34d3df1a56d2d68c4d2e31ab4f
Alex Curran <alexcurran.va@gmail.com>
2024-08-16 15:30:24 -0500

    Adjust word syntax

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@
 /** @type {import("express").RequestHandler} */
 async function getProtected(req, res, next) {
     try {
-        res.status(200).json({ message: "You successfully accessed the protected route user!" });
+        res.status(200).json({ message: "You successfully accessed the protected user route!" });
     } catch (err) {
         next(err);
     }
